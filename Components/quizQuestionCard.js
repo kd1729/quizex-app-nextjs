71dd9e5a9f62b9fd8ee0a3ab8b84f826c3cf5e0a
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-01-11 00:46:22 +0530

    Implemented nextQues and PrevQues Button

--- Components/quizQuestionCard.js
+++ Components/quizQuestionCard.js
@@ -1,35 +1,35 @@
-import React from "react";
+import React, {useRef} from "react";
 
-const QuizQuestionCard = ({ question }) => {
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+const QuizQuestionCard = ({ question, questionNo }) => {
   const answers = [...question.incorrect_answers];
   answers.push(question.correct_answer);
   answers = shuffleArray(answers);
 
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-    return array;
-  };
-
   return (
-    <div>
-      <h2 className="text-2xl font-semibold text-emerald-500">
-        {question.question}
+    <div className="w-96">
+      <h2 className="text-xl font-semibold text-emerald-800 mb-4">
+      {questionNo}. {question.question}
       </h2>
 
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center ">
         {answers.map((answer, index) => {
           return (
             <div
               key={index}
-              className="bg-slate-300 mb-4 p-8 w-[50%] border-8 rounded-lg"
+              className="bg-slate-100 mb-4 p-4 w-[50%] border-4 rounded-lg cursor-pointer"
             >
-              <p className="text-2xl font-semibold text-emerald-500">
-                {answer}
+              <p className="text-l font-semibold text-emerald-600">
+              {index+1}.  {answer}
               </p>
             </div>
           );

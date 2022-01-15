c30a2dc811fb5dcebdbdfb47cd7940041084ed15
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-01-16 01:34:21 +0530

    modified quizQuestionCard

--- Components/quizQuestionCard.js
+++ Components/quizQuestionCard.js
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React from "react";
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -10,21 +10,29 @@ function shuffleArray(array) {
   return array;
 }
 
-const QuizQuestionCard = ({ question, questionNo }) => {
+const QuizQuestionCard = ({ question, questionNo}, ref) => {
+
+  const [clicked, setClicked] = React.useState(false);
+
   const answers = [...question.incorrect_answers];
   answers.push(question.correct_answer);
   answers = shuffleArray(answers);
 
-  
-
-  
 
 
+  const checkAnswer = (answer) => {
+    setClicked(!clicked);
+    if (answer === question.correct_answer) {
+      ref.current += 4;
+    } else {
+      ref.current -= 1;
+    }
+  };
 
   return (
     <div className="w-96">
       <h2 className="text-xl font-semibold text-emerald-800 mb-4">
-      {questionNo}. {question.question}
+        {questionNo}. {question.question}
       </h2>
 
       <div className="flex flex-col justify-center ">
@@ -32,11 +40,12 @@ const QuizQuestionCard = ({ question, questionNo }) => {
           return (
             <div
               key={index}
-              className="bg-slate-100 mb-4 p-4 w-[50%] border-4 rounded-lg cursor-pointer"
-            >
-              <p className="text-l font-semibold text-emerald-600">
-              {index+1}.  {answer}
-              </p>
+              className="text-l font-semibold text-emerald-600 bg-white mb-4 p-4 w-[50%] border-4 rounded-lg cursor-pointer
+                         hover:bg-blue-500 hover:text-white active:bg-blue-700"
+              style={{ transition: "all 0.2s ease-in-out", backgroundColor: clicked ? "white" : "" }}
+              onClick={() => {checkAnswer(answer)}}
+              >
+              {index + 1}. {answer}
             </div>
           );
         })}
@@ -45,4 +54,6 @@ const QuizQuestionCard = ({ question, questionNo }) => {
   );
 };
 
-export default QuizQuestionCard;
+const forwardRef = React.forwardRef(QuizQuestionCard);
+
+export default forwardRef;
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,24 +1,19 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import QuizQuestionCard from "../Components/quizQuestionCard";
 
 const Quiz = () => {
+  const timeLeft = 10;
+  const score = useRef(0);
 
   const [state, setState] = useState({
     questions: [],
     currentQuestion: 0,
-    score: 0,
     isLoading: true,
     isFinished: false,
   });
 
-  const { questions, currentQuestion, score, isLoading, isFinished } = state;
-
-  // const [quiz, setQuiz] = useState([]);
-  // const [currentQuestion, setCurrentQuestion] = useState(0);
-  // const [score, setScore] = useState(0);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [isFinished, setIsFinished] = useState(false);
+  const { questions, currentQuestion, isLoading, isFinished } = state;
 
   const url =
     "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
@@ -48,59 +43,56 @@ const Quiz = () => {
           {isFinished ? (
             <div className="text-center">
               <h1 className="text-xl font-bold text-center text-white py-6">
-                You scored {score} out of 10
+                You scored {score.current} marks.
               </h1>
             </div>
           ) : (
             <>
+              Time : <span id="timer">{timeLeft}</span>
               <div className="bg-slate-300 p-8 border-4 rounded-lg">
                 <QuizQuestionCard
-
                   questionNo={currentQuestion + 1}
                   question={questions[currentQuestion]}
-                  state={state}
-                  setState={setState}
+                  // state={state}
+                  // setState={setState}
+                  ref={score}
                 />
               </div>
-
               <div>
                 <div className="flex justify-between mt-4">
                   {currentQuestion !== 0 ? (
-                  <button 
-                    className="bg-slate-300 p-4 border-4 rounded-lg"
-                    onClick={() => {
-                      setState({
-                        ...state,
-                        currentQuestion: currentQuestion - 1,
-                      });
-                    }}
-                  >
-                    Previous
-                  </button>
-                  ) : (
-                    <button 
-                    className="bg-transparent text-white p-4">
+                    <button
+                      className="bg-slate-300 p-4 border-4 rounded-lg hover:bg-slate-500 hover:text-white"
+                      onClick={() => {
+                        setState({
+                          ...state,
+                          currentQuestion: currentQuestion - 1,
+                        });
+                      }}
+                    >
                       Previous
                     </button>
-
+                  ) : (
+                    <button className="bg-transparent text-white p-4">
+                      Previous
+                    </button>
                   )}
 
-
                   {currentQuestion !== 9 ? (
-                  <button 
-                    className="bg-slate-300 p-4 border-4 rounded-lg"
-                    onClick={() => {
-                      setState({
-                        ...state,
-                        currentQuestion: currentQuestion + 1,
-                      });
-                    }}
-                  >
-                    Next
-                  </button>
+                    <button
+                      className="bg-slate-300 p-4 border-4 rounded-lg hover:bg-slate-500 hover:text-white"
+                      onClick={() => {
+                        setState({
+                          ...state,
+                          currentQuestion: currentQuestion + 1,
+                        });
+                      }}
+                    >
+                      Next
+                    </button>
                   ) : (
                     <button
-                      className="bg-slate-300 p-4 border-4 rounded-lg"
+                      className="bg-slate-300 p-4 border-4 rounded-lg hover:bg-slate-500 hover:text-white"
                       onClick={() => {
                         setState({
                           ...state,
@@ -109,7 +101,7 @@ const Quiz = () => {
                       }}
                     >
                       Finish
-                    </button>  
+                    </button>
                   )}
                 </div>
               </div>
@@ -122,18 +114,3 @@ const Quiz = () => {
 };
 
 export default Quiz;
-
-{
-  /* <div className=" flex flex-col justify-center items-center">
-  {quiz.map((question, index) => {
-    return (
-      <div
-        key={index}
-        className="bg-slate-300 mb-4 p-8 w-[40%] border-8 rounded-lg"
-      >
-        <QuizQuestionCard question={question} />
-      </div>
-    );
-  })}
-</div>; */
-}

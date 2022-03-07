6a104c6c25699a279941a0f93d353d65eaa99a20
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-03-07 12:06:53 +0530

    added prisma ORM and postgresql db

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,40 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import EmailProvider from "next-auth/providers/email"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+const prisma = new PrismaClient();
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+
   providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET
-      }),
-      EmailProvider({
-        server: process.env.MAIL_SERVER,
-        from: 'NextAuth.js <no-reply@example.com>'
-      }),
-    ],
-})
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    //   EmailProvider({
+    //     server: process.env.EMAIL_SERVER,
+    //     from: process.env.EMAIL_FROM,
+    //   }),
+    // ],
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+
+  secret: process.env.SECRET,
+
+  session: {
+    jwt: true,
+    maxAge: 15 * 24 * 60 * 60,
+  },
+});
